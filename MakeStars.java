f826863b017150c8c4b0e7e1ac5656385853a947
OnikuSpedu <srakibnyc1@gmail.com>
2021-01-04 13:19:11 -0500

    Working on MakeStars

---
+++ MakeStars.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MakeStars {
+    public static void main(String[] args) {
+
+        Scanner lines = new Scanner(System.in);
+
+        while (lines.hasNextLine()) {
+
+            Scanner line = new Scanner(lines.nextLine());
+            String starsLine = "";
+            while(line.hasNext()) {
+                String word = line.next();
+                String stars = "";
+                for(int i = 0; i < word.length(); i++) {
+                    stars += "*";
+                }
+                starsLine += stars;
+                if (i <= 0 && i < word.length - 1) {
+
+                }
+            }
+            System.out.println();
+            line.close();
+        }
+        lines.close();
+    }
+}
